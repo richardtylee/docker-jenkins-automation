5b5c9a52eff2db902cdbb08b1a424c3487236743
richardleebr <richardlee@bleacherreport.com>
2018-01-20 19:57:27 -0800

    Generate testapp project using mvn archetype:generate.

---
+++ testapp/src/main/java/com/richardtylee/testapp/App.java
@@ -0,0 +1,13 @@
+package com.richardtylee.testapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
